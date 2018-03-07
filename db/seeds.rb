33457e93f313698df392164cabbdc5f4f2148725
Franco Benedetti <francobenedetti2@gmail.com>
2018-03-07 11:21:55 -0300

    Categoy - User - Product

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Category.create(name: 'Guitarra Electrica')
+Category.create(name: 'Guitarra Acustica')
+Category.create(name: 'Bajo')
+Category.create(name: 'Bateria y Percusion')
+Category.create(name: 'Amplificadores')
+Category.create(name: 'Efectos y Pedales')
+Category.create(name: 'Teclados y Syntetizadores')
+Category.create(name: 'Equipos de Dj')
+Category.create(name: 'Pro Audio/Home Studio')
+Category.create(name: 'Vientos')
+Category.create(name: 'Cuerdas Frotadas')
+Category.create(name: 'Accesorios')
+Category.create(name: 'Partes')
